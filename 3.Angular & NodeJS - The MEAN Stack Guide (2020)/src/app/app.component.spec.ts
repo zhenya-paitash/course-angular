151b7ab05ad14d3f6e9512b259db0dea01380db1
zhenya-paitash <naitavv93@gmail.com>
2020-08-03 17:36:43 +0300

    udp ng cli/core

--- 3.Angular & NodeJS - The MEAN Stack Guide (2020)/src/app/app.component.spec.ts
+++ 3.Angular & NodeJS - The MEAN Stack Guide (2020)/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MEAN-stack-guide'`, () => {
+  it(`should have as title 'MEAN-guide'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MEAN-stack-guide');
+    expect(app.title).toEqual('MEAN-guide');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('MEAN-stack-guide app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MEAN-guide app is running!');
   });
 });
